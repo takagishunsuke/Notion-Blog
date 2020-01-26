38218eaa6bcbbd090d412724c7caf5663778f4f0
た か ぎ <takagishunsuke1129@gmail.com>
2020-01-26 23:55:12 +0900

    change some links

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,22 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/takagi_1129',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/takagishunsuke',
   },
   {
     Comp: LinkedIn,
     alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://www.linkedin.com/in/tkgshn/',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:takagishunsuke1129@gmail.com?subject=Notion Blog',
   },
 ]
 
@@ -43,8 +43,8 @@ export default () => (
       <h1 style={{ marginTop: 0 }}>Contact</h1>
 
       <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://zeit.co">ZEIT</ExtLink>
+        Shunsuke Takagi - UX Engineer @{' '}
+        <ExtLink href="https://hassyadai.com">Hassyadai</ExtLink>
       </div>
 
       <div className={contactStyles.links}>
