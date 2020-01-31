75fbdb20172cd51a7821c368620ff54f013e6c3b
た か ぎ <takagishunsuke1129@gmail.com>
2020-02-01 02:19:11 +0900

    delete future icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
-import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
@@ -27,7 +26,7 @@ export default () => (
         </ExtLink>
       </h2>
 
-      <Features />
+
 
       <div className="explanation">
         <p>
