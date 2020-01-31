0a0afdb282206b7c897a4f4d85d362b4f8a67f09
た か ぎ <takagishunsuke1129@gmail.com>
2020-02-01 02:14:16 +0900

    Change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,36 +31,10 @@ export default () => (
 
       <div className="explanation">
         <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://zeit.co">ZEIT</ExtLink>. It
-          leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
+17歳，N高生．「ハッシャダイ」という会社でユーザー体験をデザインすることメインとしています．スキルセットはPjM/UX/IAあたりです．高校退学→新規事業→サービスデザイン・グロースとか．新規事業をやったり，転職メディアをやったりでリサーチ・MVPの作成・情報デザイン（infomation architecture）・グロースハック・データ分析あたりだったり，UXに基づいたSNSコンテンツ戦略などをやってきました．社外ではbouncyの動画ライターや，英会話塾の「PRESENC」でネットコース新設のPMなどやったりも．今はNoCodeのbubbleで高卒に向けた進路選択をサポートするプロダクトを作っています
         </p>
 
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
-        </p>
+
       </div>
     </div>
   </>
